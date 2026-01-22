0eb32cf10f8c3e62ffdffb2c20810725ad9232d7
reyhan <ramaraihan559@gmail.com>
2026-01-22 23:00:14 +0700

    g analityc

--- resources/views/filament/ga4.blade.php
+++ resources/views/filament/ga4.blade.php
@@ -1,11 +1,16 @@
 @php($GA = config('services.ga.measurement_id'))
 
 @if($GA)
-    <script async src="https://www.googletagmanager.com/gtag/js?id={{ $GA }}"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '{{ $GA }}');
-    </script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KXRMHQL4SD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KXRMHQL4SD');
+</script>
 @endif
+
+
