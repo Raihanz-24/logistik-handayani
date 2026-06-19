68a32903ec5ec93a860dc4ae2e6132f3010470aa
reyhan <ramaraihan559@gmail.com>
2026-06-19 22:11:33 +0700

    fixing desain login

--- resources/views/filament/layouts/auth.blade.php
+++ resources/views/filament/layouts/auth.blade.php
@@ -3,7 +3,10 @@
 ])
 
 @push('styles')
-    @vite('resources/css/filament/admin/login.css')
+    <link
+        rel="stylesheet"
+        href="{{ asset('css/filament/admin/login.css') }}?v={{ filemtime(public_path('css/filament/admin/login.css')) }}"
+    />
 @endpush
 
 <x-filament-panels::layout.base :livewire="$livewire">
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/filament/admin/login.css',
                 'resources/js/app.js',
             ],
             refresh: true,
